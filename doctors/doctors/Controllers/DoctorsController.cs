65b184906314677c685cbe28f7b22d346b6dcc23
Bart Le <bart@pj.edu.pl>
2022-06-08 00:40:53 +0200

    Add initial Entity Framework Core setup

---
+++ doctors/doctors/Controllers/DoctorsController.cs
@@ -0,0 +1,19 @@
+﻿using doctors.Services;
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace doctors.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class DoctorsController : ControllerBase
+	{
+		private readonly IDatabaseService _databaseService;
+
+		public DoctorsController(IDatabaseService databaseService)
+		{
+			_databaseService = databaseService;
+		}
+	}
+}
---
+++ doctors/doctors/Models/DatabaseContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace doctors.Models
+{
+	public class DatabaseContext : DbContext
+	{
+		public DatabaseContext()
+		{
+		}
+
+		public DatabaseContext(DbContextOptions options) : base(options)
+		{
+		}
+
+		public DbSet<Doctor> Doctors { get; set; }
+		public DbSet<Medicament> Medicaments { get; set; }
+		public DbSet<Patient> Patients { get; set; }
+		public DbSet<Prescription> Prescriptions { get; set; }
+		public DbSet<PrescriptionMedicament> PrescriptionMedicaments { get; set; }
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			base.OnModelCreating(modelBuilder);
+		}
+	}
+}
