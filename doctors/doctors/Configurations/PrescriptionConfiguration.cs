fa6f82bdc61bb588439e55a57a506ee69aca8705
Bart Le <bart@pj.edu.pl>
2022-06-08 20:03:42 +0200

    Refactor initial data

--- doctors/doctors/Configurations/PrescriptionConfiguration.cs
+++ doctors/doctors/Configurations/PrescriptionConfiguration.cs
@@ -33,12 +33,7 @@ namespace doctors.Configurations
 					new Prescription { IdPrescription = 2, Date = Convert.ToDateTime("2022-01-01"), DueDate = Convert.ToDateTime("2023-01-01"), IdPatient = 2, IdDoctor = 2 },
 					new Prescription { IdPrescription = 3, Date = Convert.ToDateTime("2022-01-01"), DueDate = Convert.ToDateTime("2023-01-01"), IdPatient = 3, IdDoctor = 3 },
 					new Prescription { IdPrescription = 4, Date = Convert.ToDateTime("2022-01-01"), DueDate = Convert.ToDateTime("2023-01-01"), IdPatient = 4, IdDoctor = 4 },
-					new Prescription { IdPrescription = 5, Date = Convert.ToDateTime("2022-01-01"), DueDate = Convert.ToDateTime("2023-01-01"), IdPatient = 5, IdDoctor = 5 },
-					new Prescription { IdPrescription = 6, Date = Convert.ToDateTime("2022-01-01"), DueDate = Convert.ToDateTime("2023-01-01"), IdPatient = 1, IdDoctor = 1 },
-					new Prescription { IdPrescription = 7, Date = Convert.ToDateTime("2022-01-01"), DueDate = Convert.ToDateTime("2023-01-01"), IdPatient = 2, IdDoctor = 2 },
-					new Prescription { IdPrescription = 8, Date = Convert.ToDateTime("2022-01-01"), DueDate = Convert.ToDateTime("2023-01-01"), IdPatient = 3, IdDoctor = 3 },
-					new Prescription { IdPrescription = 9, Date = Convert.ToDateTime("2022-01-01"), DueDate = Convert.ToDateTime("2023-01-01"), IdPatient = 4, IdDoctor = 4 },
-					new Prescription { IdPrescription = 10, Date = Convert.ToDateTime("2022-01-01"), DueDate = Convert.ToDateTime("2023-01-01"), IdPatient = 5, IdDoctor = 5 }
+					new Prescription { IdPrescription = 5, Date = Convert.ToDateTime("2022-01-01"), DueDate = Convert.ToDateTime("2023-01-01"), IdPatient = 5, IdDoctor = 5 }
 				);
 		}
 	}
